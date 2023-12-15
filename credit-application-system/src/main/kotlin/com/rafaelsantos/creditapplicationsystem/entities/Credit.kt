3e121743ea6784f5ce28ebfa9af9de69f072866b
Rafael Santos <rafa.05125@gmail.com>
2023-12-15 20:52:43 -0300

    Adding class annotations

--- credit-application-system/src/main/kotlin/com/rafaelsantos/creditapplicationsystem/entities/Credit.kt
+++ credit-application-system/src/main/kotlin/com/rafaelsantos/creditapplicationsystem/entities/Credit.kt
@@ -1,16 +1,34 @@
 package com.rafaelsantos.creditapplicationsystem.entities
 
 import com.rafaelsantos.creditapplicationsystem.enums.Status
+import jakarta.persistence.*
 import java.math.BigDecimal
 import java.time.LocalDate
 import java.util.UUID
 
+@Entity
+@Table(name ="tb_credit")
 data class Credit(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
+
+    @Column(nullable = false, unique = true)
     val creditCode: UUID = UUID.randomUUID(),
+
+    @Column(nullable = false)
     val creditValue: BigDecimal = BigDecimal.ZERO,
+
+    @Column(nullable = false)
     val dayFirstInstallment: LocalDate,
+
+    @Column(nullable = false)
     val numberOfInstallElements: Int = 0,
+
+    @Enumerated
     val status: Status = Status.IN_PROGRESS,
+
+    @ManyToOne
     val customer: Customer? = null
 )
--- credit-application-system/src/main/kotlin/com/rafaelsantos/creditapplicationsystem/entities/Customer.kt
+++ credit-application-system/src/main/kotlin/com/rafaelsantos/creditapplicationsystem/entities/Customer.kt
@@ -1,12 +1,35 @@
 package com.rafaelsantos.creditapplicationsystem.entities
 
+import jakarta.persistence.*
+
+@Entity
+@Table(name = "tb_customer")
 data class Customer(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long? = null,
+
+    @Column(nullable = false)
     var firstName: String = "",
+
+    @Column(nullable = false)
     var lastName: String = "",
+
+    @Column(nullable = false, unique = true)
     val cpf: String,
+
+    @Column(nullable = false, unique = true)
     var email: String = "",
+
+    @Column(nullable = false)
     val password: String = "",
+
+    @Embedded
+    @Column(nullable = false)
     var address: Address = Address(),
+
+    @Column(nullable = false)
+    @OneToMany(fetch = FetchType.LAZY,
+        cascade = arrayOf(CascadeType.REMOVE, CascadeType.PERSIST),
+        mappedBy = "customer")
     var credits: List<Credit> = mutableListOf(),
 )
