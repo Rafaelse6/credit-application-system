b667ecefee03b59d95a80f67f68d459406b13edd
Rafael Santos <rafa.05125@gmail.com>
2023-12-16 08:58:03 -0300

    ICreditService, ICustomerService created

---
+++ credit-application-system/src/main/kotlin/com/rafaelsantos/creditapplicationsystem/services/ICreditService.kt
@@ -0,0 +1,13 @@
+package com.rafaelsantos.creditapplicationsystem.services
+
+import com.rafaelsantos.creditapplicationsystem.entities.Credit
+import java.util.UUID
+
+interface ICreditService {
+
+    fun save(credit: Credit): Credit
+
+    fun findAllByCustomer(customerId: Long): List<Credit>
+
+    fun findByCreditCode(creditCode: UUID): Credit
+}
---
+++ credit-application-system/src/main/kotlin/com/rafaelsantos/creditapplicationsystem/services/ICustomerService.kt
@@ -0,0 +1,12 @@
+package com.rafaelsantos.creditapplicationsystem.services
+
+import com.rafaelsantos.creditapplicationsystem.entities.Customer
+
+interface ICustomerService {
+
+    fun save(customer: Customer): Customer
+
+    fun findById(id: Long): Customer
+
+    fun delete(id: Long): Customer
+}
