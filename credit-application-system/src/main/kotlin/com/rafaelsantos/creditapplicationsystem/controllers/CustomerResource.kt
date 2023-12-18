74c365cd3f987e83a1bcbddc945665e6a0c68adf
Rafael Santos <rafa.05125@gmail.com>
2023-12-18 18:53:37 -0300

    Customer View created, findById and delete

--- credit-application-system/src/main/kotlin/com/rafaelsantos/creditapplicationsystem/controllers/CustomerResource.kt
+++ credit-application-system/src/main/kotlin/com/rafaelsantos/creditapplicationsystem/controllers/CustomerResource.kt
@@ -1,7 +1,12 @@
 package com.rafaelsantos.creditapplicationsystem.controllers
 
 import com.rafaelsantos.creditapplicationsystem.dto.CustomerDTO
+import com.rafaelsantos.creditapplicationsystem.dto.CustomerView
+import com.rafaelsantos.creditapplicationsystem.entities.Customer
 import com.rafaelsantos.creditapplicationsystem.services.implementations.CustomerService
+import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -18,4 +23,13 @@ class CustomerResource (
         val savedCustomer = this.customerService.save(customerDTO.toEntity())
         return "Customer ${savedCustomer.email} saved"
     }
+
+    @GetMapping("/{id}")
+    fun findById(@PathVariable id: Long) : CustomerView {
+        val customer: Customer = this.customerService.findById(id)
+        return CustomerView(customer)
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteById(@PathVariable id: Long) = this.customerService.delete(id)
 }
