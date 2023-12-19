636bdaf49dceae4361029242b7aac9f5150c0b34
Rafael Santos <rafa.05125@gmail.com>
2023-12-19 11:01:33 -0300

    CreditViewList created, findAllCustomerCredits

--- credit-application-system/src/main/kotlin/com/rafaelsantos/creditapplicationsystem/controllers/CreditResource.kt
+++ credit-application-system/src/main/kotlin/com/rafaelsantos/creditapplicationsystem/controllers/CreditResource.kt
@@ -1,12 +1,16 @@
 package com.rafaelsantos.creditapplicationsystem.controllers
 
 import com.rafaelsantos.creditapplicationsystem.dto.CreditDTO
+import com.rafaelsantos.creditapplicationsystem.dto.CreditViewList
 import com.rafaelsantos.creditapplicationsystem.entities.Credit
 import com.rafaelsantos.creditapplicationsystem.services.implementations.CreditService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.util.stream.Collectors
 
 @RestController
 @RequestMapping("/api/credits")
@@ -18,5 +22,12 @@ class CreditResource(
     fun saveCredit(@RequestBody creditDTO: CreditDTO) : String{
         val credit : Credit = this.creditService.save(creditDTO.toEntity())
         return "Credit ${credit.creditCode} - Customer ${credit.customer?.firstName} saved !"
+    }
+
+    @GetMapping
+    fun findAllByCustomerId(@RequestParam(value = "customerId") customerId : Long) : List<CreditViewList>{
+        return this.creditService.findAllByCustomer(customerId).stream()
+            .map { credit : Credit -> CreditViewList(credit) }
+            .collect(Collectors.toList())
     }
 }
