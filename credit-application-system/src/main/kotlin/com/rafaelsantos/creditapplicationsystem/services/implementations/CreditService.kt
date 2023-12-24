9b6e3c29dfad9af616927152f5d9b80a7f52ff4f
Rafael Santos <rafa.05125@gmail.com>
2023-12-23 21:44:06 -0300

    Validating first day of installment to save credit

--- credit-application-system/src/main/kotlin/com/rafaelsantos/creditapplicationsystem/services/implementations/CreditService.kt
+++ credit-application-system/src/main/kotlin/com/rafaelsantos/creditapplicationsystem/services/implementations/CreditService.kt
@@ -5,6 +5,7 @@ import com.rafaelsantos.creditapplicationsystem.exceptions.BusinessException
 import com.rafaelsantos.creditapplicationsystem.repositories.CreditRepository
 import com.rafaelsantos.creditapplicationsystem.services.ICreditService
 import org.springframework.stereotype.Service
+import java.time.LocalDate
 import java.util.*
 
 @Service
@@ -13,6 +14,7 @@ class CreditService(
     private val customerService: CustomerService
 ) : ICreditService {
     override fun save(credit: Credit): Credit {
+        this.validateFirstDayOfInstallment(credit.dayFirstInstallment)
         credit.apply {
             customer = customerService.findById(credit.customer?.id!!)
         }
@@ -27,7 +29,13 @@ class CreditService(
        val credit: Credit =
            (this.creditRepository.findByCreditCode(creditCode)
                ?: throw BusinessException("Credit $creditCode not found !"))
-        return if(credit.customer?.id == customerId) credit else throw IllegalArgumentException("Contact admin")
+        return if(credit.customer?.id == customerId) credit
+        else throw IllegalArgumentException("Contact admin")
+    }
+
+    private fun validateFirstDayOfInstallment(firstDayOfInstallment: LocalDate) : Boolean{
+        return if (firstDayOfInstallment.isBefore(LocalDate.now().plusMonths(3))) true
+        else throw BusinessException("Invalid Date")
     }
 
 }
