0456f714237e46a947b373a006abc539707d136a
Rafael Santos <rafa.05125@gmail.com>
2023-12-24 11:13:30 -0300

    Save Customer test

--- credit-application-system/src/test/kotlin/com/rafaelsantos/creditapplicationsystem/service/CustomerServiceTest.kt
+++ credit-application-system/src/test/kotlin/com/rafaelsantos/creditapplicationsystem/service/CustomerServiceTest.kt
@@ -1,10 +1,65 @@
 package com.rafaelsantos.creditapplicationsystem.service
 
+import com.rafaelsantos.creditapplicationsystem.entities.Address
+import com.rafaelsantos.creditapplicationsystem.entities.Customer
+import com.rafaelsantos.creditapplicationsystem.repositories.CustomerRepository
+import com.rafaelsantos.creditapplicationsystem.services.implementations.CustomerService
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
+import io.mockk.verify
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mock
 import org.springframework.test.context.ActiveProfiles
+import java.math.BigDecimal
 
 @ActiveProfiles("test")
 @ExtendWith(MockKExtension::class)
 class CustomerServiceTest {
+
+    @MockK lateinit var customerRepository: CustomerRepository
+    @InjectMockKs lateinit var customerService: CustomerService
+
+    @Test
+    fun `should create customer`(){
+
+        //given
+        val fakeCustomer : Customer = buildCustomer()
+        every { customerRepository.save(any()) } returns fakeCustomer
+
+        //when
+        val actual : Customer = customerService.save(fakeCustomer)
+
+        //then
+        Assertions.assertThat(actual).isNotNull
+        Assertions.assertThat(actual).isSameAs(fakeCustomer)
+        verify(exactly = 1) { customerRepository.save(fakeCustomer) }
+    }
+
+    private fun buildCustomer(
+        firstName: String = "Cami",
+        lastName: String = "Cavalcante",
+        cpf: String = "28475934625",
+        email: String = "camila@gmail.com",
+        password: String = "12345",
+        zipCode: String = "12345",
+        street: String = "Rua da Cami",
+        income: BigDecimal = BigDecimal.valueOf(1000.0),
+        id: Long = 1L
+    ) = Customer(
+        firstName = firstName,
+        lastName = lastName,
+        cpf = cpf,
+        email = email,
+        password = password,
+        address = Address(
+            zipCode = zipCode,
+            street = street,
+        ),
+        income = income,
+        id = id
+    )
 }
