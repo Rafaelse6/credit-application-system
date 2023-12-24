93c790fb28a796b0eef7716747cfc7c3ca9960ec
Rafael Santos <rafa.05125@gmail.com>
2023-12-24 11:36:42 -0300

    Delete Customer test

--- credit-application-system/src/test/kotlin/com/rafaelsantos/creditapplicationsystem/service/CustomerServiceTest.kt
+++ credit-application-system/src/test/kotlin/com/rafaelsantos/creditapplicationsystem/service/CustomerServiceTest.kt
@@ -9,6 +9,8 @@ import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.junit5.MockKExtension
+import io.mockk.just
+import io.mockk.runs
 import io.mockk.verify
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
@@ -70,6 +72,22 @@ class CustomerServiceTest {
             .isThrownBy { customerService.findById(fakeId) }
             .withMessage("Id $fakeId not found!")
         verify(exactly = 1)  {customerRepository.findById(fakeId)}
+    }
+
+    @Test
+    fun `should delete customer by id`(){
+        //given
+        val fakeId : Long = Random().nextLong()
+        val fakeCustomer : Customer = buildCustomer()
+        every { customerRepository.findById(fakeId) } returns Optional.of(fakeCustomer)
+        every { customerRepository.delete(fakeCustomer) } just runs
+
+        //when
+        customerService.delete(fakeId)
+
+        //then
+        verify(exactly = 1)  {customerRepository.findById(fakeId)}
+        verify(exactly = 1) { customerRepository.delete(fakeCustomer) }
     }
 
     private fun buildCustomer(
