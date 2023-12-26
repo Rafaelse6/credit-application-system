4cf62893fb42f23165a4949b0f7af00d3f6868cb
Rafael Santos <rafa.05125@gmail.com>
2023-12-26 06:56:45 -0300

    Persisting data with TestEntityManager

--- credit-application-system/src/test/kotlin/com/rafaelsantos/creditapplicationsystem/repository/CreditRepositoryTest.kt
+++ credit-application-system/src/test/kotlin/com/rafaelsantos/creditapplicationsystem/repository/CreditRepositoryTest.kt
@@ -1,19 +1,72 @@
 package com.rafaelsantos.creditapplicationsystem.repository
 
+import com.rafaelsantos.creditapplicationsystem.entities.Address
+import com.rafaelsantos.creditapplicationsystem.entities.Credit
+import com.rafaelsantos.creditapplicationsystem.entities.Customer
+import com.rafaelsantos.creditapplicationsystem.repositories.CreditRepository
 import com.rafaelsantos.creditapplicationsystem.repositories.CustomerRepository
+import org.junit.jupiter.api.BeforeEach
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager
 import org.springframework.test.context.ActiveProfiles
+import java.math.BigDecimal
+import java.time.LocalDate
 
 @ActiveProfiles("test")
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class CreditRepositoryTest {
     @Autowired
-    lateinit var customerRepository: CustomerRepository
+    lateinit var creditRepository: CreditRepository
 
     @Autowired
     lateinit var testEntityManager: TestEntityManager
+
+    private lateinit var customer: Customer
+    private lateinit var credit1: Credit
+    private lateinit var credit2: Credit
+
+    @BeforeEach fun setup(){
+        customer = testEntityManager.persist(buildCustomer())
+        credit1 = testEntityManager.persist(buildCredit(customer = customer))
+        credit2 = testEntityManager.persist(buildCredit(customer = customer))
+    }
+
+    private fun buildCredit(
+        creditValue: BigDecimal = BigDecimal.valueOf(100.0),
+        dayFirstInstallment: LocalDate = LocalDate.now().plusMonths(2L),
+        numberOfInstallments: Int = 15,
+        customer: Customer = buildCustomer()
+    ): Credit = Credit(
+        creditValue = creditValue,
+        dayFirstInstallment = dayFirstInstallment,
+        numberOfInstallments = numberOfInstallments,
+        customer = customer
+    )
+
+    private fun buildCustomer(
+        firstName: String = "Cami",
+        lastName: String = "Cavalcante",
+        cpf: String = "28475934625",
+        email: String = "camila@gmail.com",
+        password: String = "12345",
+        zipCode: String = "12345",
+        street: String = "Rua da Cami",
+        income: BigDecimal = BigDecimal.valueOf(1000.0),
+        id: Long = 1L
+    ) = Customer(
+        firstName = firstName,
+        lastName = lastName,
+        cpf = cpf,
+        email = email,
+        password = password,
+        address = Address(
+            zipCode = zipCode,
+            street = street,
+        ),
+        income = income,
+        id = id
+    )
 }
